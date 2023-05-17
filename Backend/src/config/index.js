c09661b01eac6113bf099f4f3da2351463eff13d
Suman Bhandari <sumanbhandari9860@gmail.com>
2023-05-17 18:45:48 +0530

    Ecommerce baics

---
+++ Backend/src/config/index.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const config = {
+    PORT: process.env.PORT || 3000,
+    MONGODB_URL:process.env.MONGODB_URL || "mongodb+srv://sumanbhandari9860:bhairav98@cluster0.rm3ttbx.mongodb.net/?retryWrites=true&w=majority"
+    
+}
+
+export default config
